fdeb9a32bff4d3bf38f80cd5c51268b457733294
onebronze467 <onebronze@naver.com>
2026-07-31 15:03:43 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -39,13 +39,17 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.destination === "image") {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     }),
   );
 });
-
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches
